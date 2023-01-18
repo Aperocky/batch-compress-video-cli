61277a74a3cfa6032ce39ef0a3babe9f7c9cae21
Rocky Li <aperocky@gmail.com>
2023-01-18 17:57:06 -0500

    Add guardrail for cli, delete-original flag, and readme. bump version to 2.0

--- src/video_compress.py
+++ src/video_compress.py
@@ -39,12 +39,13 @@ class VideoCompressor:
         video_stream = video_streams[0]
         return int(video_stream["width"]), int(video_stream["height"])
 
-    def __init__(self, source=None, destination=None, process_dir=None, crf=23, preset="slower", scale=1):
+    def __init__(self, source=None, destination=None, process_dir=None, crf=23, preset="slower", scale=1, delete_original=False):
         self.crf = crf
         self.preset = preset
         self.get_directories(source, destination, process_dir)
         self.targets = []
         self.scale = scale
+        self.delete_original = delete_original
 
     def get_directories(self, source, destination, process_dir):
         current_directory = os.getcwd()
@@ -74,31 +75,44 @@ class VideoCompressor:
             self.process_target(target)
 
     def process_target(self, target):
+        compressed_target_name = re.sub(".mp4$", "", target, flags=re.I) + "_compressed.mp4"
+        target_path = os.path.join(self.source, target)
+        process_path = os.path.join(self.process_dir, target)
+        process_output_path = os.path.join(self.process_dir, compressed_target_name)
+        end_path = os.path.join(self.destination, compressed_target_name)
+
         # Step 1. move video to processing directory if needed.
         print("======== PROCESSING VIDEO: {} ========".format(target))
-        target_path = os.path.join(self.source, target)
         print(VideoCompressor.ffprobe(target_path))
-        process_path = os.path.join(self.process_dir, target)
         if target_path != process_path:
             print("copying {} to {} for processing".format(target_path, process_path))
             shutil.copy(target_path, process_path)
+
         # Step 2. compress the video in process directory.
         print("compressing video ...")
         try:
-            tmp_processed_path = self.compress_video(process_path)
+            self.compress_video(process_path, process_output_path)
         except Exception as e:
             print("PROCESSING FAILED for {}".format(process_path))
             raise e
         if self.source != self.process_dir:
-            os.remove(process_path) # Remove copied and processed material.
-        # Step 3. move compressed video to destination directory
-        if self.process_dir != self.destination:
-            compressed_target_name = re.sub(".mp4$", "", target, flags=re.I) + "_compressed.mp4"
-            end_path = os.path.join(self.destination, compressed_target_name)
-            print("copying {} to destination {}".format(process_path, end_path))
-            shutil.move(tmp_processed_path, end_path)
+            os.remove(process_path)
 
-    def compress_video(self, video_path, **kwargs):
+        # Step 3. Move compressed video to destination directory and verify/delete original
+        if process_output_path != end_path:
+            print("copying {} to destination {}".format(process_output_path, end_path))
+            shutil.move(process_output_path, end_path)
+        if not os.path.isfile(end_path):
+            raise "Error: video not compressed correctly"
+        print("Video compressed to {}".format(end_path))
+        print(VideoCompressor.ffprobe(end_path))
+        if self.delete_original:
+            print("Deleting original video: {}".format(target_path))
+            os.remove(target_path)
+        print()
+
+
+    def compress_video(self, target_path, output_path, **kwargs):
         arg_dict = {
             "crf": self.crf,
             "preset": self.preset,
@@ -107,7 +121,7 @@ class VideoCompressor:
         }
         arg_dict.update(kwargs)
         if self.scale != 1:
-            original_width, original_height = VideoCompressor.get_dimension(video_path)
+            original_width, original_height = VideoCompressor.get_dimension(target_path)
             new_width = int(original_width * self.scale)
             new_height = int(original_height * self.scale)
             if new_width % 2 != 0:
@@ -115,11 +129,9 @@ class VideoCompressor:
             if new_height % 2 != 0:
                 new_height += 1
             arg_dict["vf"] = "scale={}:{}".format(new_width, new_height)
-        output_path = re.sub(".mp4$", "", video_path, flags=re.I) + "_compressed.mp4"
-        work = ffmpeg.input(video_path)
+        work = ffmpeg.input(target_path)
         work = ffmpeg.output(work, output_path, **arg_dict)
         work.run()
-        return output_path
 
 
 SOURCE_HELP = """
@@ -135,15 +147,21 @@ PROC_HELP = """
 This is used when videos are stored in HDDs and ffmpeg would run into buffer issues, moving it to local SSD drive will speed up processing. All videos processed will be removed from the directory.
 """
 
+DELETE_HELP = """
+[bool] Delete the original file after it is compressed, default to False.
+"""
+
 CRF_HELP = """
 [int] CRF Value, higher is more compression and less quality, saner values range between 17-28, with 17 being nearly lossless.
 """
 
+PRESET_VALUES = ["veryfast", "faster", "fast", "medium", "slow", "slower", "veryslow"]
+
 PRESET_HELP = """
 [string] The speed of which to generate the output video, slower makes video smaller without compromising on quality.
-Options are [veryfast, faster, fast, medium, slow, slower, veryslow]
+Options are {}
 Default to slower
-"""
+""".format(PRESET_VALUES)
 
 SCALE_HELP = """
 [float] Scale factor, 0.5 means the video will be scaled down to 50%% of the original width and height.
@@ -154,12 +172,26 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--source', action='store', dest='source', default=None, help=SOURCE_HELP)
     parser.add_argument('-t', '--destination', action='store', dest='destination', default=None, help=DEST_HELP)
-    parser.add_argument('-p', '--process-dir', action='store', dest='process', default=None, help=PROC_HELP)
+    parser.add_argument('-p', '--process-dir', action='store', dest='process_dir', default=None, help=PROC_HELP)
+    parser.add_argument('-d', '--delete-original', action='store_true', dest='delete_orig', default=False, help=DELETE_HELP)
     parser.add_argument('--crf', action='store', dest='crf', default=23, type=int, help=CRF_HELP)
     parser.add_argument('--preset', action='store', dest='preset', default="slower", help=PRESET_HELP)
     parser.add_argument('--scale', action='store', dest='scale', default=1.0, type=float, help=SCALE_HELP)
     args = parser.parse_args()
-    compressor = VideoCompressor(args.source, args.destination, args.process, args.crf, args.preset, args.scale)
+    # arg validation.
+    if args.source and not os.path.isdir(args.source):
+        raise "Source must be a valid directory"
+    if args.destination and not os.path.isdir(args.destination):
+        raise "Destination must be a valid directory"
+    if args.process_dir and not os.path.isdir(args.process_dir):
+        raise "Process directory must be a valid directory"
+    if args.crf < 15 or args.crf > 30:
+        raise "CRF value should be between 15 and 30"
+    if args.scale < 0.1 or args.scale > 2:
+        raise "Scale must be between 0.1 and 2"
+    if not args.preset in PRESET_VALUES:
+        raise "preset values must be one of {}".format(PRESET_VALUES)
+    compressor = VideoCompressor(args.source, args.destination, args.process_dir, args.crf, args.preset, args.scale, args.delete_orig)
     compressor.run()
 
 
--- tst/test_compressor.py
+++ tst/test_compressor.py
@@ -7,6 +7,7 @@ from src.video_compress import VideoCompressor
 TEST_DIRECTORY = "tst/asset"
 DEST_DIRECTORY = "tst/destination"
 PROC_DIRECTORY = "tst/process"
+DELETE_TEST_DIRECTORY = "tst/deltest"
 TEST_VIDEO_PATH = "tst/asset/test_1.mp4"
 TEST_VIDEO_PATH_2 = "tst/asset/test_2.mp4"
 TEST_CREATE_VIDEO_PATH = "tst/asset/test_1_compressed.mp4"
@@ -17,20 +18,20 @@ TEST_DEST_VIDEO_PATH_2 = "tst/destination/test_2_compressed.mp4"
 
 @pytest.fixture
 def compressor():
+    os.mkdir(DEST_DIRECTORY)
+    os.mkdir(PROC_DIRECTORY)
     yield VideoCompressor(source=TEST_DIRECTORY, preset="medium")
     if os.path.isfile(TEST_CREATE_VIDEO_PATH):
         os.remove(TEST_CREATE_VIDEO_PATH)
     if os.path.isfile(TEST_CREATE_VIDEO_PATH_2):
         os.remove(TEST_CREATE_VIDEO_PATH_2)
-    if os.path.isdir(DEST_DIRECTORY):
-        shutil.rmtree(DEST_DIRECTORY)
-    if os.path.isdir(PROC_DIRECTORY):
-        shutil.rmtree(PROC_DIRECTORY)
+    shutil.rmtree(DEST_DIRECTORY)
+    shutil.rmtree(PROC_DIRECTORY)
 
 
 def test_compress_video(compressor):
     original_size = os.path.getsize(TEST_VIDEO_PATH)
-    compressor.compress_video(TEST_VIDEO_PATH)
+    compressor.compress_video(TEST_VIDEO_PATH, TEST_CREATE_VIDEO_PATH)
     assert(os.path.isfile(TEST_CREATE_VIDEO_PATH))
     compressed_size = os.path.getsize(TEST_CREATE_VIDEO_PATH)
     assert(original_size > compressed_size)
@@ -38,27 +39,25 @@ def test_compress_video(compressor):
 
 def test_compress_video_args(compressor):
     original_size = os.path.getsize(TEST_VIDEO_PATH)
-    compressor.compress_video(TEST_VIDEO_PATH, preset="medium")
+    compressor.compress_video(TEST_VIDEO_PATH, TEST_CREATE_VIDEO_PATH, preset="medium")
     original_size = os.path.getsize(TEST_VIDEO_PATH)
     less_compressed_size = os.path.getsize(TEST_CREATE_VIDEO_PATH)
     os.remove(TEST_CREATE_VIDEO_PATH)
 
-    compressor.compress_video(TEST_VIDEO_PATH, crf=28, preset="slower")
+    compressor.compress_video(TEST_VIDEO_PATH, TEST_CREATE_VIDEO_PATH, crf=28, preset="slower")
     more_compressed_size = os.path.getsize(TEST_CREATE_VIDEO_PATH)
     os.remove(TEST_CREATE_VIDEO_PATH)
     assert(original_size > less_compressed_size)
     assert(less_compressed_size > more_compressed_size)
 
 
-def test_scale_down_video():
+def test_scale_down_video(compressor):
     scale_compressor = VideoCompressor(source=TEST_DIRECTORY, scale=0.5)
     scale_compressor.run()
     ow, oh = VideoCompressor.get_dimension(TEST_VIDEO_PATH)
     nw, nh = VideoCompressor.get_dimension(TEST_CREATE_VIDEO_PATH)
     assert(ow == 2*nw) # this isn't strictly always true for all video
     assert(oh == 2*nh) # as resized video dimension have to be even.
-    os.remove(TEST_CREATE_VIDEO_PATH)
-    os.remove(TEST_CREATE_VIDEO_PATH_2)
 
 
 def test_compressor_targets(compressor):
@@ -82,7 +81,6 @@ def test_compressor_same_dir(compressor):
 
 
 def test_compressor_destination_dir(compressor):
-    os.mkdir(DEST_DIRECTORY)
     test_dest_compressor = VideoCompressor(source=TEST_DIRECTORY, destination=DEST_DIRECTORY, preset="medium")
     test_dest_compressor.run()
     compressor.run()
@@ -98,8 +96,6 @@ def test_compressor_destination_dir(compressor):
 
 
 def test_compressor_process_dir(compressor):
-    os.mkdir(DEST_DIRECTORY)
-    os.mkdir(PROC_DIRECTORY)
     test_proc_compressor = VideoCompressor(source=TEST_DIRECTORY, destination=DEST_DIRECTORY, process_dir=PROC_DIRECTORY, preset="medium")
     test_proc_compressor.run()
     compressor.run()
@@ -114,3 +110,12 @@ def test_compressor_process_dir(compressor):
     assert(orig_comp_size_1 == dest_comp_size_1)
     assert(orig_comp_size_2 == dest_comp_size_2)
 
+
+def test_compressor_delete_original(compressor):
+    shutil.copytree(TEST_DIRECTORY, DELETE_TEST_DIRECTORY)
+    test_del_compressor = VideoCompressor(source=DELETE_TEST_DIRECTORY, delete_original=True)
+    test_del_compressor.run()
+    del_dir_files = os.listdir(DELETE_TEST_DIRECTORY)
+    assert(len(del_dir_files) == 3)
+    assert(all(["compressed" in f for f in del_dir_files if f.endswith(".mp4")]))
+    shutil.rmtree(DELETE_TEST_DIRECTORY)
